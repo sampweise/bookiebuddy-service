4de5e775b22f0be6faa13ce11213d793f2a019ce
sampweise <sampweise@gmail.com>
2022-09-25 16:37:55 -0500

    Add Comment and Clean Up

--- database/utils.ts
+++ database/utils.ts
@@ -1,10 +1,25 @@
 import { MongoClient } from "mongodb";
 
+/*Build Base Backend Requirement 7:
+    -Description: This functions connects with the mongodb database in the authorization user database
+    -Author: Sam Weise
+    -Created: 9/18/22
+    -Revisions: 9/25/22-Sam Weise
+    -Preconditions: (databaseName: string, collectionName: Context) database and collection name for the collection
+                    you wish to connect to
+    -Postconditions: (Promise<Collection<Document>>) which is promise containing the collection object
+    -Errors: If the connection to the database fails or if the user send an incorrect payload the errors response
+             is a 500
+*/
 export const connect = async (databaseName: string, collectionName: string) => {
+    //Grab connection string environment variable
     const connectionString = process.env["bookiebuddydb"];
-    console.log(connectionString);
+    //Connect to the monog cluster
     const cluster = await MongoClient.connect(connectionString);
+    //Connect to the database specified in the args
     const database = cluster.db(databaseName)
+    //Connect to the collection specified in the args
     const collection = database.collection(collectionName);
+    //Return a resolved promise
     return Promise.resolve(collection);
 }
--- get-user/handler.ts
+++ get-user/handler.ts
@@ -1,22 +1,37 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 import { getUser } from "../database/usersDatabase"
 
+/*Build Base Backend Requirement 7:
+    -This functions connects with the mongodb database in the authorization user database
+     and then returns that information to the endpoint.
+    -Cade Sarkin
+    -Created: 9/20/22
+    -Revisions: 9/21/22-Cade Sarkin, 9/25/22-Sam Weise
+    -Preconditions: (context: Context, req: HttpRequest) the context for the function and the request sent across
+    -Postconditions: (Promise<void>) because the function returns the information through the context
+    -Errors: If the connection to the database fails or if the user send an incorrect payload the errors response
+             is a 500
+*/
 const handler: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
     try{
+        //Store email from url query param
         const email: string = req.query.email;
         console.log("email: ", email);
+        //Call the getUser function and store the response 
         const response = await getUser(email, context);
+        //Return the response to the end point
         context.res = {
             body: response
         };
     }
     catch (e)
     {
-        context.log(e);
+        context.log(e.message);
+        //Return a 500 error with the body being the error message returned.
         context.res = {
             status: 500,
-            body: e
+            body: e.message
         };
     }
 };
